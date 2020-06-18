90a96db5b2bccece8a3bf07f2665ab7b567bb6b3
zxz <1365818536@qq.com>
2020-06-19 00:44:41 +0800

    搜索栏样式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,13 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
+	<view class="container">
+		<!-- 搜索栏 -->
+		<view class="search">
+			<view class="input-box">
+				<input type="text"/>
+			</view>
 		</view>
+		
+	    <!-- 搜索的结果 -->
 	</view>
 </template>
 
@@ -24,19 +28,12 @@
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
+.search .input-box {
+  background-color: #EB4450;
+  padding: 20rpx;
+}
+.search .input-box input {
+  background-color: #fff;
+}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
 </style>
